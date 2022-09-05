9c110bcf1f78f217e1a89c7021c5876b6514ebd5
dennisnyarko <dennis_nyarko@outlook.com>
2022-09-05 18:27:42 +0000

    create object to store title & post

--- app.js
+++ app.js
@@ -34,7 +34,12 @@ app.get("/compose", function(req, res){
 }); 
 
 app.post("/compose", function(req, res){
-  console.log(req.body.postTitle);
+  var post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
+
+
 })
 
 
